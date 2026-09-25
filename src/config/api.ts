9388de5e47e59bf721d7c03b5d3dae08538bb11d
Yash Soni <yashsony23478@gmail.com>
2026-09-25 22:35:19 +0530

    chore(config): update cloudflare tunnel API URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -11,6 +11,6 @@ export const ENV = {
   // Internet Cloudflare Tunnel for Port 8001:
   // 1. Double-click `start_app_tunnel.bat`
   // 2. Paste generated HTTPS URL below and append `/api/v1`
-  API_BASE_URL: 'https://establish-marathon-father-agreed.trycloudflare.com/api/v1',
+  API_BASE_URL: 'https://orchestra-corporations-remaining-bless.trycloudflare.com/api/v1',
 };
 
